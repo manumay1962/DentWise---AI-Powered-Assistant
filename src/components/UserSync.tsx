b76180a67c7e2c4146daba54220d4bbb9d2d73bf
manumay@1962 <manumay8@gmail.com>
2025-11-15 16:20:34 +0530

    usersync added

---
+++ src/components/UserSync.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import { syncUser } from '@/lib/actions/users';
+import { useUser } from '@clerk/nextjs'
+import { useEffect } from 'react';
+
+
+function UserSync() {
+
+    const {isLoaded,isSignedIn} = useUser();
+
+
+    useEffect(()=>{
+        const handleUserSync = async () =>{
+            if(isLoaded && isSignedIn){
+                try {
+                    await syncUser();
+                } catch (error) {
+                    console.log("Error syncing user on client:", error);
+                    
+                }
+            }
+        }
+        handleUserSync();
+    },[isLoaded,isSignedIn]);
+
+    return null;
+}
+
+export default UserSync
